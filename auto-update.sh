b2eeaf443285274368501e88d11669c95e694e3c
pux <nick-ng@users.noreply.github.com>
2025-11-21 21:38:42 +1300

    feat: add link to github readme once update is done

--- auto-update.sh
+++ auto-update.sh
@@ -10,5 +10,6 @@ for ((i = 0 ; i < 50 ; i++)); do
 	git push
 
 	echo "updated at $(date). waiting 30 minutes before next update"
+	echo "visit https://github.com/nick-ng/poe-map-team?tab=readme-ov-file#merclab to see what is profitable in merclab"
 	sleep 30m
 done
