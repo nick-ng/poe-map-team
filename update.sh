8c00d33a4ba1f953726c4d16a1a78f32f53d44e7
pux <nick-ng@users.noreply.github.com>
2026-03-09 18:57:28 +1300

    fix: add space between merclab and heist

--- update.sh
+++ update.sh
@@ -7,8 +7,8 @@ echo "Getting transfigured gem prices"
 node ./js/lab-gems.js
 cat ./LAB_GEMS.md >> README.md
 
-cat ./other-notes/heist.md >> README.md
 echo "" >> README.md
+cat ./other-notes/heist.md >> README.md
 
 git add ./LAB_GEMS.md
 git add ./README.md
